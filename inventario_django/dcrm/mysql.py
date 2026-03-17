afe41935434e874d33c52556df7ff9d319773a5b
lunavhoyos <lunavhoyos@gmail.com>
2026-03-17 10:53:08 -0500

    subiendo lo que llevo de django

---
+++ inventario_django/dcrm/mysql.py
@@ -0,0 +1,10 @@
+import mysql.connector
+dataBase = mysql.connector.connect(
+    host = "localhost",
+    user = "Luna_18",
+    passwd = "15miope2008")
+ 
+# Se crrea un cursor para recorrer las filas y las columnas de la base de datos
+cursorObject = dataBase.cursor()#Se creea un cursor para ejecutar comandos SQL
+cursorObject.execute("CREATE DATABASE cliente")#Se ejecuta el comando SQL para crear la base de datos llamada "cliente"
+print("Base de datos creada con exito")#Se imprime un mensaje de confimacion
