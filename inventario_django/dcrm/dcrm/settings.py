3e0925814b10588a000ebb3e6738520e3092b77c
lunavhoyos <lunavhoyos@gmail.com>
2026-05-26 06:56:11 -0500

    Cambios con csrf_token

--- inventario_django/dcrm/dcrm/settings.py
+++ inventario_django/dcrm/dcrm/settings.py
@@ -131,3 +131,7 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*.loca.lt']
+
+CSRF_TRUSTED_ORIGINS = ['http://*.loca.lt']
